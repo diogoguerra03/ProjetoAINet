8bf29037bc04e9d03c07a56a06fe65f88a6cc1f9
Rafael Santos <rafaelfsantos03@gmail.com>
2023-06-04 18:35:09 +0100

    Update Forgot Password Form
    Atualizei o design do formulario Forgot Password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,9 +2,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mb-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-5 mt-5">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
@@ -16,13 +16,12 @@
                     @endif
 
                     <form method="POST" action="{{ route('password.email') }}">
+                        <img class="rounded-5 mx-auto d-block" style="height: 200px;" src="{{ asset('assets/images/logos/imagineshirt.png') }}" alt="Shirt Image">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-6 mx-auto">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email Adress" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -33,8 +32,8 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="d-flex">
+                                <button type="submit" class="btn btn-primary mx-auto btn-lg">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
