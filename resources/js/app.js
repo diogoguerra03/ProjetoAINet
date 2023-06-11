72a1dcca1e8b79dfeeea2a5870f5c4318087de36
Rafael Santos <rafaelfsantos03@gmail.com>
2023-06-12 00:35:37 +0100

    Dashboard
    continuacao da dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -49,30 +49,26 @@ var ctxB = document.getElementById("barChart").getContext('2d');
 var myBarChart = new Chart(ctxB, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Number of orders',
+            data: [12, 19, 3, 5, 2, 3,32, 12, 19, 3, 5, 2],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgb(42,107,229,0.5)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgb(42,107,229)'
             ],
             borderWidth: 1
         }]
     },
     options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Orders per month'
+            }
+        },
         scales: {
             yAxes: [{
                 ticks: {
@@ -82,4 +78,46 @@ var myBarChart = new Chart(ctxB, {
         }
     }
 });
+
+
+//piechart
+    var ctxP = document.getElementById("pieChart").getContext('2d');
+    var myPieChart = new Chart(ctxP, {
+        type: 'pie',
+        data: {
+            labels: [
+                'Eletrônicos',
+                'Roupas',
+                'Livros',
+                'Brinquedos',
+                'Outros',
+            ],
+            datasets: [{
+                data: [300, 50, 100, 40, 120],
+                backgroundColor: [
+                    'rgb(42,107,229,0.5)',
+                    'rgba(145,60,76,0.5)',
+                    'rgba(42,229,67,0.5)',
+                    'rgba(229,204,42,0.5)',
+                    'rgba(229,42,42,0.5)',
+                ],
+                hoverBackgroundColor: [
+                    'rgb(42,107,229,0.9)',
+                    'rgba(145,60,76,0.9)',
+                    'rgba(42,229,67,0.9)',
+                    'rgba(229,204,42,0.9)',
+                    'rgba(229,42,42,0.9)',
+                ]
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Categories sold'
+                }
+            },
+            responsive: true
+        }
+    });
 
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,10 +4,68 @@
 @extends('layouts.app')
 
 @section('content')
-    teste1
-    <canvas id="barChart"></canvas>
-    teste
-    
+    <!-- ========================= CARDS ========================= -->
+    <div class="row mb-5">
+        <div class="col-md-3">
+            <div class="card h-100 bg-info text-white">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">Total Products</h5>
+                    <p class="card-text">3928</p>
+                    <hr>
+                    <a href="" class="btn btn-light mt-auto">More info</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card h-100 bg-success text-white">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">Orders placed</h5>
+                    <p class="card-text">293</p>
+                    <hr>
+                    <a href="" class="btn btn-light mt-auto">More info</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card h-100 bg-danger text-white">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">Money Earned</h5>
+                    <p class="card-text">1120€</p>
+                    <hr>
+                    <a href="" class="btn btn-light mt-auto">More info</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card h-100 bg-warning text-white">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">Costumers registered</h5>
+                    <p class="card-text">120</p>
+                    <hr>
+                    <a href="" class="btn btn-light mt-auto">More info</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ========================= GRAPHS ========================= -->
+    <div class="row">
+        <div class="col-md-7">
+            <div class="card pb-5">
+                <canvas id="barChart"></canvas>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card pb-1">
+                <canvas id="pieChart" width="200" height="200"></canvas>
+            </div>
+    </div>
+    </div>
+
+
+
+
+
     </div>
     </div>
     </div>
