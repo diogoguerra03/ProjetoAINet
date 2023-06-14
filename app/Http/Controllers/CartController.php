4c260d9217de5a112631380550785fea2054b769
Lucas Remigio <2211034@my.ipleiria.pt>
2023-06-14 23:23:55 +0100

    carrinho faz qualquer coisa

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,29 +5,52 @@ namespace App\Http\Controllers;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
     public function show(): View
     {
-        $cart = session('cart', []);
-        return view('cart.show', compact('cart'));
+        $cart = Session::get('cart', []);
+        return view('cart.show', ['cart' => $cart]);
 
     }
 
-    public function addToCart(Request $request) : RedirectResponse
+    public function addToCart(Request $request): RedirectResponse
     {
 
-        // Retrieve the submitted form data
-        $tshirtImage = $request->session()->get('orderItem');
-        $size = $request->session()->get('size');
-        $color = $request->session()->get('color');
-        $quantity = $request->session()->get('quantity');
 
-        // Implement your cart logic here, such as storing the item in the cart session or database
+        $productId = $request->input('orderItem');
+        $size = $request->input('size');
+        $color = $request->input('color');
+        $quantity = $request->input('quantity');
 
-        // Optionally, you can redirect the user to the cart page or a success message
-        return redirect()->route('cart.show')->with('success', 'Item added to cart successfully');
+        $item = [
+            'product_id' => $productId,
+            'quantity'   => $quantity,
+            'color'      => $color,
+            'size'       => $size,
+        ];
+
+        // Adicionar o item ao carrinho na sessão
+        $cart = Session::get('cart', []);
+        $cart[] = $item;
+        Session::put('cart', $cart);
+
+        // Redirecionar de volta ao catálogo ou a outra página
+        return redirect()->back()->with('success', 'Item adicionado ao carrinho com sucesso.');
+
+    }
+
+    public function removeFromCart($index)
+    {
+        $cart = Session::get('cart', []);
+
+        if (isset($cart[$index])) {
+            unset($cart[$index]);
+            Session::put('cart', $cart);
+        }
+
+        return redirect()->back()->with('success', 'Item removido do carrinho com sucesso.');
     }
 }
-
--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -12,78 +12,25 @@
 
 @section('main')
     <div>
-        <h3>Disciplinas no carrinho</h3>
+        <h3>T-shirts no carrinho</h3>
     </div>
-    @if ($cart)
-        {{-- @include('disciplinas.shared.table', [
-            'disciplinas' => $cart,
-            'showCurso' => true,
-            'showDetail' => true,
-            'showEdit' => false,
-            'showDelete' => false,
-            'showRemoveCart' => true,
-        ]) --}}
-        @foreach ($orderItem as $item)
-            <tr>
-                <td>{{ $item->tshirt_image_id }}</td>
-                <td>{{ $disciplina->color_code }}</td>
-                <td>{{ $disciplina->size }}</td>
-                {{-- @if ($showCurso)
-                    <td>{{ $disciplina->cursoRef->tipo }} - {{ $disciplina->cursoRef->nome }}</td>
-                @endif
-                <td>{{ $disciplina->ano }}</td>
-                <td>{{ $disciplina->semestreStr }}</td>
-                @if ($showDetail)
-                    <td class="button-icon-col"><a class="btn btn-secondary"
-                            href="{{ route('disciplinas.show', ['disciplina' => $disciplina]) }}">
-                            <i class="fas fa-eye"></i></a></td>
-                @endif
-                @if ($showEdit)
-                    <td class="button-icon-col"><a class="btn btn-dark"
-                            href="{{ route('disciplinas.edit', ['disciplina' => $disciplina]) }}">
-                            <i class="fas fa-edit"></i></a></td>
-                @endif
-                @if ($showDelete)
-                    <td class="button-icon-col">
-                        <form method="POST" action="{{ route('disciplinas.destroy', ['disciplina' => $disciplina]) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" name="delete" class="btn btn-danger">
-                                <i class="fas fa-trash"></i></button>
-                        </form>
-                    </td>
-                @endif
-                @if ($showAddCart ?? false)
-                    <td class="button-icon-col">
-                        <form method="POST" action="{{ route('cart.add', ['disciplina' => $disciplina]) }}">
-                            @csrf
-                            <button type="submit" name="addToCart" class="btn btn-success">
-                                <i class="fas fa-plus"></i></button>
-                        </form>
-                    </td>
-                @endif
-                @if ($showRemoveCart ?? false)
-                    <td class="button-icon-col">
-                        <form method="POST" action="{{ route('cart.remove', ['disciplina' => $disciplina]) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" name="removeFromCart" class="btn btn-danger">
-                                <i class="fas fa-remove"></i></button>
-                        </form>
-                    </td>
-                @endif --}}
-            </tr>
-        @endforeach
-        <div class="my-4 d-flex justify-content-end">
-            <button type="submit" class="btn btn-primary" name="ok" form="formStore">Confirmar Compra</button>
-            <button type="submit" class="btn btn-danger ms-3" name="clear" form="formClear">Limpar Carrinho</button>
-        </div>
-        <form id="formStore" method="POST" action="{{ route('cart.store') }}" class="d-none">
-            @csrf
-        </form>
-        <form id="formClear" method="POST" action="{{ route('cart.destroy') }}" class="d-none">
-            @csrf
-            @method('DELETE')
-        </form>
-    @endif
+    @foreach ($cart as $item)
+        <tr>
+            <td>{{ $item->color }}</td>
+            <td>{{ $item->size }}</td>
+            <td>{{ $item->quantity }}</td>
+            <td>{{ $item->product_id }}</td>
+        </tr>
+    @endforeach
+    <div class="my-4 d-flex justify-content-end">
+        <button type="submit" class="btn btn-primary" name="ok" form="formStore">Confirmar Compra</button>
+        <button type="submit" class="btn btn-danger ms-3" name="clear" form="formClear">Limpar Carrinho</button>
+    </div>
+    {{-- <form id="formStore" method="POST" action="{{ route('cart.store') }}" class="d-none">
+        @csrf
+    </form>
+    <form id="formClear" method="POST" action="{{ route('cart.destroy') }}" class="d-none"> --}}
+    @csrf
+    @method('DELETE')
+    </form>
 @endsection
